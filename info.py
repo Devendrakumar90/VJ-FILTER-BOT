3debcc11f76fe1b467f117063862e30e7221915b
anujmore90 <166386367+anujmore90@users.noreply.github.com>
2025-01-15 21:47:54 +0530

    Update info.py

--- info.py
+++ info.py
@@ -90,7 +90,7 @@ PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', '') # Public Channel Us
 # Links
 GRP_LNK = environ.get('GRP_LNK', 'https://t.me/cinema_house_dis')
 CHNL_LNK = environ.get('CHNL_LNK', 'https://cinema_house_13')
-SUPPORT_CHAT = environ.get('SUPPORT_CHAT', '@cinema_house_dis) # Support Chat Link Without https:// or @
+SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'cinema_house_dis) # Support Chat Link Without https:// or @
 OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/anujmore96k')
 
 # True Or False
