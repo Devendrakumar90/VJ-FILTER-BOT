5595f0fbb85cc9b0938146f2368168ccdce1e123
anujmore90 <166386367+anujmore90@users.noreply.github.com>
2025-01-18 10:12:07 +0530

    Update info.py

--- info.py
+++ info.py
@@ -90,13 +90,13 @@ PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', '') # Public Channel Us
 # Links
 GRP_LNK = environ.get('GRP_LNK', 'https://t.me/cinema_house_dis')
 CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/cinema_house_update')
-SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'https://t.me/cinema_house_chat') # Support Chat Link Without https:// or @
+SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'https://t.me/cinema_house_update') # Support Chat Link Without https:// or @
 OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/its_anuj_123')
 
 # True Or False
 AI_SPELL_CHECK = bool(environ.get('AI_SPELL_CHECK', True))
 PM_SEARCH = bool(environ.get('PM_SEARCH', True))
-BUTTON_MODE = bool(environ.get('BUTTON_MODE', False))
+BUTTON_MODE = bool(environ.get('BUTTON_MODE', True))
 MAX_BTN = bool(environ.get('MAX_BTN', True))
 IS_TUTORIAL = bool(environ.get('IS_TUTORIAL', False))
 IMDB = bool(environ.get('IMDB', False))
