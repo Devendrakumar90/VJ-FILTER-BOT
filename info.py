d2741bb1b4b6843909c6bbebdbc96ccc954f679d
anujmore90 <166386367+anujmore90@users.noreply.github.com>
2025-01-20 10:57:13 +0530

    Update info.py

--- info.py
+++ info.py
@@ -26,7 +26,7 @@ auth_users = [int(user) if id_pattern.search(user) else user for user in environ
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
 
 # This Channel Is For When User Start Your Bot Then Bot Send That User Name And Id In This Log Channel, Same For Group Also.
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', ' -1002340192761'))
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', ' -1002482323036'))
 
 # This Is File Channel Where You Upload Your File Then Bot Automatically Save It In Database 
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002498430033').split()]  # For Multiple Id Use One Space Between Each.
@@ -125,7 +125,7 @@ VERIFY_SND_SHORTLINK_API = environ.get('VERIFY_SND_SHORTLINK_API', '')
 
 
 # Shortlink Info
-SHORTLINK_MODE = bool(environ.get('SHORTLINK_MODE', True)) # Set True Or False
+SHORTLINK_MODE = bool(environ.get('SHORTLINK_MODE', False)) # Set True Or False
 SHORTLINK_URL = environ.get('SHORTLINK_URL', 'https://shortlink.com/')
 SHORTLINK_API = environ.get('SHORTLINK_API', 'https://speedlinkurl.com/f6694cc21dbc1b4727e32ce3d27ab61893ec5277')
 TUTORIAL = environ.get('TUTORIAL', 'https://youtube.com/shorts/u3jJPRWxS7s?si=TCJjHGU-EEFj-0_a') # How Open Shortner Link Video Link , Channel Link Where You Upload Your Video.
