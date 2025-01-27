52a30d7d1b889e230cc7a8d134a7e675af9190ca
anujmore90 <166386367+anujmore90@users.noreply.github.com>
2025-01-27 17:55:22 +0530

    Update info.py

--- info.py
+++ info.py
@@ -13,7 +13,7 @@ id_pattern = re.compile(r'^.\d+$')
 SESSION = environ.get('SESSION', 'TechVJBot')
 API_ID = int(environ.get('API_ID', '24719696'))
 API_HASH = environ.get('API_HASH', '862d5b32031f8a9b9a547aca40ef1640')
-BOT_TOKEN = environ.get('BOT_TOKEN', "")
+BOT_TOKEN = environ.get('BOT_TOKEN', "8097788870:AAF_Akaun85R--9G6ZeSHE-27YVyVC1pJfA")
 
 
 # This Pictures Is For Start Message Picture, You Can Add Multiple By Giving One Space Between Each.
