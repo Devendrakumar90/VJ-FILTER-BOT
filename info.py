e740cedf98d87671660a814598fde00ad6dd1d70
Abhishek Kumar <198420887+jonsina8@users.noreply.github.com>
2025-02-27 10:44:08 +0530

    Update info.py

--- info.py
+++ info.py
@@ -126,7 +126,7 @@ VERIFY_SND_SHORTLINK_API = environ.get('VERIFY_SND_SHORTLINK_API', '')
 
 # Shortlink Info
 SHORTLINK_MODE = bool(environ.get('SHORTLINK_MODE', True)) # Set True Or False
-SHORTLINK_URL = environ.get('SHORTLINK_URL', 'arlinks.in')
+SHORTLINK_URL = environ.get('SHORTLINK_URL', 'https://indiaearnx.com')
 SHORTLINK_API = environ.get('SHORTLINK_API', '4648f3b9a330674316e0cf01a7b5e8611e4fcd8a')
 TUTORIAL = environ.get('TUTORIAL', 'https://t.me/how_to_downlodm/13') # How Open Shortner Link Video Link , Channel Link Where You Upload Your Video.
 
