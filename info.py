ce72f6df719cdb2d1b4b3cb35e76af49dca317b0
Abhishek Kumar <198420887+jonsina8@users.noreply.github.com>
2025-04-05 21:55:50 +0530

    Update info.py

--- info.py
+++ info.py
@@ -113,8 +113,8 @@ USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
 
 # Token Verification Info :
 VERIFY = bool(environ.get('VERIFY', True))
-VERIFY_SHORTLINK_URL = environ.get('VERIFY_SHORTLINK_URL', 'api.modijiurl.com')
-VERIFY_SHORTLINK_API = environ.get('VERIFY_SHORTLINK_API', '7db07214bfe669e3520f6277fe46f982ee4ec018')
+VERIFY_SHORTLINK_URL = environ.get('VERIFY_SHORTLINK_URL', 'brieflyurl.com')
+VERIFY_SHORTLINK_API = environ.get('VERIFY_SHORTLINK_API', 'd35593bdd2032426cb96230d85a18082a1ace7a7')
 VERIFY_TUTORIAL = environ.get('VERIFY_TUTORIAL', 'https://t.me/how_to_downlodm/15')
 
 # If You Fill Second Shortner Then Bot Attach Both First And Second Shortner And Use It For Verify.
