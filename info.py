92438f4bbb4e79320bd80d3b62609dd48d1472e2
Abhishek Kumar <198420887+jonsina8@users.noreply.github.com>
2025-04-07 18:53:06 +0530

    Update info.py

--- info.py
+++ info.py
@@ -21,7 +21,7 @@ PICS = (environ.get('PICS', 'https://envs.sh/pRi.jpg')).split()
 
 
 # Admins & Users
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1133549523 1807927334').split()] # For Multiple Id Use One Space Between Each.
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '7446337623 1807927334').split()] # For Multiple Id Use One Space Between Each.
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]  # For Multiple Id Use One Space Between Each.
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
 
@@ -91,7 +91,7 @@ PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', '') # Public Channel Us
 GRP_LNK = environ.get('GRP_LNK', 'https://t.me/+Ak9u-iW4Lps5MjFl')
 CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/Hindi_movie_uplod')
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'https://t.me/Hindi_movie_uplod') # Support Chat Link Without https:// or @
-OWNER_LNK = environ.get('OWNER_LNK', 'http://t.me/Abhikumar7799')
+OWNER_LNK = environ.get('OWNER_LNK', 'http://t.me/Abhikumar779')
 
 # True Or False
 AI_SPELL_CHECK = bool(environ.get('AI_SPELL_CHECK', False))
